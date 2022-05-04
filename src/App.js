bd7bb3c665312780b142d743a6c78ca1c6d60196
JhonZafra <jezortiz26@gmail>
2022-05-04 11:09:29 -0500

    Agregar imagenes

--- src/App.js
+++ src/App.js
@@ -1,74 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./styles/App.css";
+import BorderCollie from './media/BorderCollie.avif';
+import Rhodesian from './media/Rhodesian.jpg';
+import GermanSheaperd from './media/GermanSheaperd.avif';
+import Doberman from './media/Doberman.jpg';
+
+
 
 function App() {
   return (
     <div className="App">
       <header>
-        <ul class='navbar'>
+        <ul className="navbar">
           <li>
-            <img src="./media/logo.jpg" alt="Imagen de un perrito" class='logo'/>
-              </li>
-          <li>
-            <button class=" button mainButton">Nuevo Post</button>
+            <img
+              src="./media/logo.jpg"
+              alt="Imagen de un perrito"
+              className="logo"
+            />
           </li>
           <li>
-            <div class="buscar">
-              <input placeholder="Buscar una raza"/>
-              <i class="fa-solid fa-magnifying-glass button iconobusqueda"></i>
+            <button className=" button mainButton">+ Nuevo Post</button>
+          </li>
+          <li>
+            <div className="buscar">
+              <input placeholder="Buscar una raza" />
+              <i className="fa-solid fa-magnifying-glass button iconobusqueda"></i>
             </div>
           </li>
           <li>
-            <button class="button secondaryButton">Inicio de sesión</button>
+            <button className="button secondaryButton">Inicio de sesión</button>
           </li>
           <li>
-            <button class="button mainButton">Registro</button>
+            <button className="button mainButton">Registro</button>
           </li>
         </ul>
       </header>
       <main>
         <section>
-            <div class="breedTitle title">
-                <h1>Razas de Perros</h1>
-            </div>
-            <ul class="breedCardContainer">
-                <li class="breedCard">
-                    <div class="contenedorImagen">
-                        <img src="./media/BorderCollie.avif" alt="Foto del border coli"/>
-                    </div>
-                    <span class="breedTitle">
-                        Border Collie
-                    </span>
-                </li>
-                <li class="breedCard">
-                    <div class="contenedorImagen">
-                        <img src="./media/GermanSheaperd.avif" alt="Foto del border coli"/>
-                    </div>
-                    <span class="breedTitle">
-                        German Sheaperd
-                    </span>
-                </li>
-                <li class="breedCard">
-                    <div class="contenedorImagen">
-                        <img src="./media/Rhodesian.jpg" alt="Foto del border coli"/>
-                    </div>
-                    <span class="breedTitle">
-                        Rhodesian
-                    </span>
-                </li>
-                <li class="breedCard">
-                    <div class="contenedorImagen">
-                        <img src="./media/Doberman.jpg" alt="Foto del border coli"/>
-                    </div>
-                    <span class="breedTitle">
-                        Doberman
-                    </span>
-                </li>
-            </ul>
+          <div className="breedTitle title">
+            <h1>Razas de Perros</h1>
+          </div>
+          <ul className="breedCardContainer">
+            <li className="breedCard">
+              <div className="contenedorImagen">
+                <img
+                  src={BorderCollie}
+                  alt="Foto del border coli"
+                />
+              </div>
+              <span className="breedTitle">Border Collie</span>
+            </li>
+            <li className="breedCard">
+              <div className="contenedorImagen">
+                <img
+                  src={GermanSheaperd}
+                  alt="Foto del border coli"
+                />
+              </div>
+              <span className="breedTitle">German Sheaperd</span>
+            </li>
+            <li className="breedCard">
+              <div className="contenedorImagen">
+                <img src={Rhodesian} alt="Foto del border coli" />
+              </div>
+              <span className="breedTitle">Rhodesian</span>
+            </li>
+            <li className="breedCard">
+              <div className="contenedorImagen">
+                <img src={Doberman} alt="Foto del border coli" />
+              </div>
+              <span className="breedTitle">Doberman</span>
+            </li>
+          </ul>
         </section>
         <section></section>
       </main>
-    <footer></footer>
+      <footer></footer>
     </div>
   );
 }
