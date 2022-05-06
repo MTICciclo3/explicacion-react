4d6cd645ee855906af51582077f85e7b7d6d5be5
JhonZafra <jezortiz26@gmail>
2022-05-06 11:19:24 -0500

    Crear función de card raza de perros

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import logo from "./media/logo.jpg";
+import "./styles/App.css";
+import BorderCollie from "./media/BorderCollie.avif";
+import Rhodesian from "./media/Rhodesian.jpg";
+import GermanSheaperd from "./media/GermanSheaperd.avif";
+import Doberman from "./media/Doberman.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <ul className="navbar">
+          <li>
+            <img src={logo} alt="Imagen de un perrito" className="logo" />
+          </li>
+          <li>
+            <button className=" button mainButton">+ Nuevo Post</button>
+          </li>
+          <li>
+            <div className="buscar">
+              <input placeholder="Buscar una raza" />
+              <i className="fa-solid fa-magnifying-glass button iconobusqueda"></i>
+            </div>
+          </li>
+          <li>
+            <button className="button secondaryButton">Inicio de sesión</button>
+          </li>
+          <li>
+            <button className="button mainButton">Registro</button>
+          </li>
+        </ul>
+      </header>
+      <main>
+        <section>
+          <div className="breedTitle title">
+            <h1>Razas de Perros</h1>
+          </div>
+          <ul className="breedCardContainer">
+            <CardRazasPerros nombreRaza="Boder Collie" imagen={BorderCollie} />
+            <CardRazasPerros nombreRaza="Rhodesian" imagen={Rhodesian} />
+            <CardRazasPerros nombreRaza="German Sheaperd" imagen={GermanSheaperd} />
+            <CardRazasPerros nombreRaza="Doberman" imagen={Doberman} />
+          </ul>
+        </section>
+        <section></section>
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
+
+function CardRazasPerros(props) {
+  return (
+    <li className="breedCard">
+      <div className="contenedorImagen">
+        <img src={props.imagen} alt={props.nombreRaza} />
+      </div>
+      <span className="breedTitle">{props.nombreRaza}</span>
+    </li>
+  );
+}
+
+export default App;
